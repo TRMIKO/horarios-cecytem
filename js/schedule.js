45fb43319a011c1e32c713a548affc324e5cab8d
arturo guerrero <trmiko8@gmail.com>
2017-12-07 01:47:49 -0600

    asteriscos

--- js/schedule.js
+++ js/schedule.js
@@ -1,28 +1,33 @@
 var alertify = require("alertifyjs")
+function salir() {
+    alertify.error("¡Nos vemos pronto!");
+    setTimeout(function () {
+        location.href = "signin.html";
+    }, 3000);
+}
 Vue.component('horario', {
   props: ['horas', 'hora'],
 
   template: '<tr >' +
-    ' <td> <button v-if="vacio(horas[1])"  class="button">{{horas[1]}}</button> </td> ' +
-    ' <td> <button v-if="vacio(horas[2])"  class="button">{{horas[2]}}</button> </td> ' +
-    ' <td> <button v-if="vacio(horas[3])"  class="button">{{horas[3]}}</button> </td> ' +
-    ' <td> <button v-if="vacio(horas[4])"  class="button">{{horas[4]}}</button> </td> ' +
-    ' <td> <button v-if="vacio(horas[5])"  class="button">{{horas[5]}}</button> </td> ' +
+    ' <td>{{horas["GEN"]}}</td> ' +
+    ' <td>{{horas["TURNO"]}}</td> ' +
+    ' <td>{{horas["ESP"]}}</td> ' +
+    ' <td>{{horas["GRUPO"]}}</td> ' +
+    '<td> <span><a class="button is-danger" v-on:click="remove(p)"  href="#"><span class="icon"><i class="fa fa-ban"></i></span></a></span> </td>' +
+    '<td> <span><a class="button is-info" v-on:click="update(p)"  href="#"><span class="icon"><i class="fa fa-pencil"></i></span></a></span> </td>' +
+    '<td> <span><a class="button " v-on:click="pdf(p)"  href="#"><span class="icon"><i class="fa  fa-file-pdf-o "></i></span></a></span> </td>' +
     '</tr>',
-  methods: {
-    emit: function(index, day) {
-      this.$emit('remove-class', index, day)
-    },
-    vacio:function(a){
-      if (a!=''){
-        return true
+    methods: {
+      remove: function(p) {
+        this.$emit('remove',p['INT_USER'])
+      },
+      update: function(p) {
+        this.$emit('update',p)
+      },pdf: function(p) {
+        this.$emit('pdf',p)
+      }
 
-      }
-      else {
-        return false
-      }
     }
-  }
 })
 var app = new Vue({
   el: '#app',
@@ -39,7 +44,7 @@ var app = new Vue({
       database: 'CECYTEM',
       port: 3306
     })
-    var q = con.query('SELECT N.VCH_NOMBRE,G.ENM_TURNO,G.VCH_ESPACIALIDAD,G.VCH_NOMBRE FROM  (TBL_HORARIO H JOIN TBL_GRUPO G ON H.INT_GRUPO=G.INT_GRUPO JOIN TBL_GENERACION N ON N.INT_GENERACION=H.INT_GENERACION)  GROUP BY G.VCH_NOMBRE,N.VCH_NOMBRE,G.ENM_TURNO,G.VCH_ESPACIALIDAD ORDER BY N.VCH_NOMBRE DESC', function(error, result) {
+    var q = con.query('SELECT N.VCH_NOMBRE AS GEN,N.INT_GENERACION AS IDG,G.ENM_TURNO AS TURNO,G.VCH_ESPACIALIDAD AS ESP,G.VCH_NOMBRE AS GRUPO,G.INT_GRUPO IDGR FROM  (TBL_HORARIO H JOIN TBL_GRUPO G ON H.INT_GRUPO=G.INT_GRUPO JOIN TBL_GENERACION N ON N.INT_GENERACION=H.INT_GENERACION)  GROUP BY G.VCH_NOMBRE,N.VCH_NOMBRE,G.ENM_TURNO,G.VCH_ESPACIALIDAD,N.INT_GENERACION,G.INT_GRUPO ORDER BY N.VCH_NOMBRE DESC', function(error, result) {
       if (error) {
         throw error;
       } else {
